7a3ee3278b16375cd826b1328a2bb5f309f2b0b8
Jacob Frazer <jakefrazer1997@googlemail.com>
2023-06-18 16:15:11 +0100

    Styling hex input

--- src/Components/ColourSelect.tsx
+++ src/Components/ColourSelect.tsx
@@ -17,12 +17,29 @@ const CombinedColor = () => {
     let calced_colours = generateAnalogousColors(c)
     setColours(calced_colours)
   }
+
+  const pickerStyles = {
+    padding: '10px 0 30px 0',
+  }
+
+  const inputStyles = {
+    width: '200px',
+    height: '40px',
+    padding: '5px',
+    borderRadius: '20px',
+    border: 'none',
+    backgroundColor: 'transparent',
+    color: colours[1],
+    outline: 'none',
+    boxShadow: '0 0 5px rgba(255, 255, 255, 0.5)',
+    fontSize: '16px',
+    
+  };
+
   return (
     <>
-      <HexColorPicker color={colours[0]} onChange={gen_set_colours} />
-      {/* 
-      <HexColorInput color={colour} onChange={setColour} />
-      */}
+      <HexColorPicker color={colours[0]} onChange={gen_set_colours} style={pickerStyles}/>
+      <HexColorInput color={colours[0]} onChange={gen_set_colours} prefixed={true} style={inputStyles}/>
     </>
   
   );
